188070ecf1a896f959a3e05f7927bd2f515a9738
Melissa Chaparro <melissa.l.chaparro@gmail.com>
2016-09-14 12:16:04 -0700

    Add columns to join tables

--- small-business/db/migrate/20160914184936_create_company_districts.rb
+++ small-business/db/migrate/20160914184936_create_company_districts.rb
@@ -1,6 +1,8 @@
 class CreateCompanyDistricts < ActiveRecord::Migration[5.0]
   def change
     create_table :company_districts do |t|
+      t.integer :company_id
+      t.integer :district_id
 
       t.timestamps
     end
