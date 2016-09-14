e93d37afc24de50e5c8b7dd66c61e068a966d30b
Melissa Chaparro <melissa.l.chaparro@gmail.com>
2016-09-14 11:42:50 -0700

    Add columns to tables

--- small-business/db/migrate/20160914170916_create_companies.rb
+++ small-business/db/migrate/20160914170916_create_companies.rb
@@ -1,7 +1,17 @@
 class CreateCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :companies do |t|
-
+      t.string :dbe
+      t.string :sbe
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :contact
+      t.string :email
+      t.string :phone
+      
       t.timestamps
     end
   end
