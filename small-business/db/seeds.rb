5749fa5ac3661d39cf5d68cdf1fedb37b45f3804
Melissa Chaparro <melissa.l.chaparro@gmail.com>
2016-09-15 11:59:30 -0700

    Seed District database

--- small-business/db/seeds.rb
+++ small-business/db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'roo'
+# require 'roo'
+
+districts = {"1": "Del Norte, Humboldt, Lake, Mendocino","2": "Lassen,  Modoc,  Plumas,  Shasta,  Siskiyou,  Tehama, Trinity","3": "Butte, Colusa, El Dorado, Glenn, Nevada, Placer, Sacramento, Sierra, Sutter, Yolo, Yuba","4": "Alameda, Contra Costa, Marin, Napa, San Franciso, San Mateo, Santa Clara, Solano, Sonoma","5": "Monterey, San Benito, San Luis Obispo, Santa Barbara, Santa Cruz","6": "Fresno, Kern, Kings, Madera, Tulare","7": "Los Angeles, Ventura","8": "Riverside, San Bernardino","9": "Inyo, Mono, Kern","10": "Alpine, Amador, Calaveras, Mariposa, Merced, San Joaquin, Stanislaus, Tuolumne","11": "San Diego, Imperial","12": "Orange"}
+
+districts.each do |number, region|
+  District.create(number: number,region: region)
+end
